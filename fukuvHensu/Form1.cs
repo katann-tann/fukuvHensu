a4010980b4beff3eb478cfb74d1bf0421a018d6c
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-06-16 14:13:26 +0900

    おまけ

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,6 +12,8 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +21,8 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += int.Parse(textBox1.Text);
-            label1.Top += int.Parse(textBox2.Text);
+            label1.Left += vx;
+            label1.Top += vy;
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -37,32 +39,34 @@ namespace fukuvHensu
         {
          
             MessageBox.Show("岩立一樹");
+            timer1.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-5";
-            textBox2.Text = "-5";
+            vx = 0;
+            vy = -5;
             label1.Text = "↑";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "5";
-            textBox2.Text = "0";
+            vx = 5;
+            vy = 0;
             label1.Text = "→";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-5";
-            textBox2.Text = "0";
+            vx = -5;
+            vy = 0;
             label1.Text = "←";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "5"; textBox2.Text = "5";
+            vx = 0;
+            vy = 5;
             label1.Text = "↓";
         }
     }
